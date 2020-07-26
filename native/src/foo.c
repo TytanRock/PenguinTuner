26a6a8565b0da6a9aedef206994496e82efe8d92
TytanRock <nesscory@gmail.com>
2020-07-25 20:17:18 -0400

    First Commit
    Adding the basics to get a working GUI

---
+++ native/src/foo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "foo.h"
+
+void bar(){
+    printf("World!\n");
+}
---
+++ native/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "foo.h"
+
+int main() {
+    printf("Hello\n");
+    bar();
+    return 0;
+}
