5806857f58309934800db0a910a70f5cc7bd621c
TytanRock <nesscory@gmail.com>
2020-08-29 14:34:46 -0400

    Updated install script to add gpg key & install

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 curl -s --compressed "https://penguintuner.tytan.rocks/KEY.gpg" | sudo apt-key add -
 sudo curl -s --compressed -o /etc/apt/sources.list.d/penguintuner.list "https://penguintuner.tytan.rocks/penguintuner.list"
 sudo apt update
+sudo apt install penguintuner
 
