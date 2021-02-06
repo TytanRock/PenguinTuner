88be7a029bff12035f42ad9f56cb77cebe49a3f3
TytanRock <coryness@tytan.rocks>
2021-02-05 21:53:32 -0500

    Scripts now properly build amd64 & armhf

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-mkdir build/$TARGET_ARCHITECTURE -p
-cd build/$TARGET_ARCHITECTURE
-
-if ($ON_DOCKER == 'true') then
-	cmake ../.. -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE
-else
-	export TARGET_ARCHITECTURE=linux-x64
-	cmake ..
-fi
-make
+rm -rf build
+meson setup build
+cd build
+ninja
 
--- cross-build.sh
+++ cross-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm -rf arm
-meson setup --cross-file armhf.ini --cross-file cross.ini arm
+rm -rf build
+meson setup --cross-file armhf.ini --cross-file cross.ini build
+cd build
+meson configure -Dhost_arch=armhf
+ninja
 
