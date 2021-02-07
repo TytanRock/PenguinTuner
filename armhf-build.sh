572c787ad5e89b383d426785547ab0d45a676d8d
TytanRock <coryness@tytan.rocks>
2021-02-06 19:43:30 -0500

    More work on cross-compile

--- armhf-build.sh
+++ armhf-build.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-rm -rf build
 meson setup --cross-file armhf.ini --cross-file cross.ini build
 cd build
 meson configure -Dhost_arch=armhf
-ninja
+meson compile
 
--- package.sh
+++ package.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-./build.sh
-cd build
-cpack
+dpkg-buildpackage
 
