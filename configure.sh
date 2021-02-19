40dab1b36dbcc37be0e4dde8987352a1425d7c1d
TytanRock <coryness@tytan.rocks>
2021-02-19 09:08:58 -0500

    Added -y to configure.sh

--- configure.sh
+++ configure.sh
@@ -4,6 +4,6 @@
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 
 apt-get update
-apt-get install libcurl4-openssl-dev libgtk-3-dev libssh-dev python3-pip build-essential meson
+apt-get install -y libcurl4-openssl-dev libgtk-3-dev libssh-dev python3-pip build-essential meson
 pip3 install --user ninja
 
