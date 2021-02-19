c3f59dabf5bddafd39eb83c51da4fbf40576cb71
TytanRock <coryness@tytan.rocks>
2021-02-19 09:20:06 -0500

    Trying to pip install with no-cache

--- configure.sh
+++ configure.sh
@@ -5,5 +5,5 @@
 
 apt-get update
 apt-get install -y libcurl4-openssl-dev libgtk-3-dev libssh-dev python3-pip build-essential meson
-pip3 install --user ninja
+pip3 install --user --no-cache-dir ninja
 
