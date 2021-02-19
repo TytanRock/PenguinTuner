b862551b4c0ccabe7149e3ad83442cc9ca76e087
TytanRock <coryness@tytan.rocks>
2021-02-18 20:58:03 -0500

    Updated configure to ask for root & install build-essential

--- configure.sh
+++ configure.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get install libcurl4-openssl-dev libgtk-3-dev libssh-dev python3-pip meson
+# If we're not root, prompt for root to install everything
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
+
+apt-get update
+apt-get install libcurl4-openssl-dev libgtk-3-dev libssh-dev python3-pip build-essential meson
 pip3 install --user ninja
 
