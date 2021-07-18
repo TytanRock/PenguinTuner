2ef80c953a617cf13ef36a9e6e3cc8ddb00f38ea
TytanRock <coryness@tytan.rocks>
2021-07-18 15:44:02 -0400

    Moved root check for linux only

--- configure.sh
+++ configure.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# If we're not root, prompt for root to install everything
-[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
-
 case "$(uname -s)" in
     Darwin*)
         brew update
@@ -10,6 +7,8 @@ case "$(uname -s)" in
         pip3 install meson ninja
         ;;
     Linux*)
+        # If we're not root, prompt for root to install everything
+        [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
         apt-get update
         apt-get install -y libcurl4-openssl-dev libgtk-3-dev libssh-dev build-essential meson ninja-build
         ;;
