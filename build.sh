ff7ea7aedf44a80ffbfccdff3ec91e0ae9ec003a
TytanRock <coryness@tytan.rocks>
2021-07-18 15:15:06 -0400

    Updated build files for macos build

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+case "$(uname -s)" in
+    Darwin*) arch=macos;;
+    Linux*)  arch="$(dpkg --print-architecture)"
+esac
+
+echo ${arch}
+
 rm -rf build
-meson setup -Darchitecture=$(dpkg --print-architecture) build
+meson setup -Darchitecture=${arch} build
 cd build
 ninja
