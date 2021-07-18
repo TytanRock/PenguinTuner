76302dffb3ae7eb911297247252d5a00040505a1
TytanRock <coryness@tytan.rocks>
2021-07-18 15:41:44 -0400

    Fixed typo in macos lib name, and added macos to checkout-build check

--- configure.sh
+++ configure.sh
@@ -3,6 +3,16 @@
 # If we're not root, prompt for root to install everything
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 
-apt-get update
-apt-get install -y libcurl4-openssl-dev libgtk-3-dev libssh-dev build-essential meson ninja-build
-
+case "$(uname -s)" in
+    Darwin*)
+        brew update
+        brew install pkgconfig cmake python3 curl libssh gtk+3
+        pip3 install meson ninja
+        ;;
+    Linux*)
+        apt-get update
+        apt-get install -y libcurl4-openssl-dev libgtk-3-dev libssh-dev build-essential meson ninja-build
+        ;;
+    *)
+        echo "Unknown Architecture, cannot configure"
+esac
