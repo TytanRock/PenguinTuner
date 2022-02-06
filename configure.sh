7a498cb039ee1e353f78918eb14399766212a53d
TytanRock <coryness@tytan.rocks>
2022-02-06 15:16:35 -0500

    Fixed up configure script & yml

--- configure.sh
+++ configure.sh
@@ -3,14 +3,14 @@
 case "$(uname -s)" in
     Darwin*)
         brew update
-        brew install pkgconfig cmake python3 curl libssh gtk+3
+        brew install pkgconfig cmake python3 curl libssh gtk+4
         pip3 install meson ninja
         ;;
     Linux*)
         # If we're not root, prompt for root to install everything
         [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
         apt-get update
-        apt-get install -y libcurl4-openssl-dev libgtk-3-dev libssh-dev build-essential meson ninja-build
+        apt-get install -y libcurl4-openssl-dev libgtk-4-dev libssh-dev build-essential meson ninja-build
         ;;
     *)
         echo "Unknown Architecture, cannot configure"
